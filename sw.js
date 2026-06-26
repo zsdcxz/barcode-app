4de9fb2a71ef2657b54d9f90f331f8d1fac3845a
SurfacePro11 <zsdcxz@naver.com>
2026-06-26 20:38:05 +0900

    feat: 라이브 스캔 전면/후면 카메라 전환 기능 추가 (v2.3)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'barcode-v12';
+const CACHE = 'barcode-v13';
 const URLS = [
   './', './index.html', './manifest.json',
   'https://cdnjs.cloudflare.com/ajax/libs/jsbarcode/3.11.6/JsBarcode.all.min.js',
