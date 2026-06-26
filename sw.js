3f71b337543d9648feb37d3f4b014a83b22e02fc
SurfacePro11 <zsdcxz@naver.com>
2026-06-26 12:19:55 +0900

    fix: 서비스워커 캐시 버전 v3으로 업데이트

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'barcode-v2';
+const CACHE = 'barcode-v3';
 const URLS = [
   './', './index.html', './manifest.json',
   'https://cdnjs.cloudflare.com/ajax/libs/jsbarcode/3.11.6/JsBarcode.all.min.js',
